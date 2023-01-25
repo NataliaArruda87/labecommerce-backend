d51d56a8f8ac0e8b78327475aa269d932f31d2c3
unknown <natalia.arruda87@gmail.com>
2023-01-24 23:14:25 -0300

    add de mais endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { users, products, purchases, getAllUsers,getAllProducts } from "./database";
+import { users, products, purchases, getAllPurchasesFromUserId } from "./database";
 import express, { Request, Response } from "express";
 import cors from "cors"
 import { TProduct, TPurchase, TUser } from "./types";
@@ -89,7 +89,82 @@ app.post('/purchases', (req: Request, res: Response) => {
     res.status(201).send("Compra realizado com sucesso!")
 })
 
+//Get Products by Id
+app.get('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id
 
+    const result = products.find((product) => product.id === id)
+
+    res.status(200).send(result)
+})
+
+// Get Purchases by user Id
+app.get('/users/:id/purchases', (req: Request, res: Response) => {
+    const id = req.params.id
+
+    res.status(200).send(getAllPurchasesFromUserId(id))
+})
+
+//Delete User by Id
+app.delete('/users/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const indexToRemove = users.findIndex((user) => user.id === id)
+    
+    if (indexToRemove >= 0) {
+        users.splice(indexToRemove, 1)
+    }
+    res.status(200).send("Usuário deletado com sucesso")
+})
+
+//Delete Product by Id
+app.delete('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const indexToRemove = products.findIndex((product) => product.id === id)
+    
+    if (indexToRemove >= 0) {
+        products.splice(indexToRemove, 1)
+    }
+    res.status(200).send("Produto deletado com sucesso")
+})
+
+//Edit User By Id
+app.put('/users/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const newEmail = req.body.email as string | undefined
+    const newPassword = req.body.password as string | undefined
+
+    const user = users.find((user) => user.id === id)
+    
+    if(user) {
+        user.email = newEmail || user.email
+        user.password = newPassword || user.password
+    }
+    
+    res.status(200).send("Atualização realizada com sucesso")
+})
+
+// Get Product By id
+app.put('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const newName = req.body.name as string | undefined
+    const newPrice = req.body.price as number | undefined
+    const newCategory = req.body.category as string | undefined
+
+
+    const product = products.find((product) => product.id === id)
+    
+    if(product) {
+        product.name = newName || product.name
+        product.category = newCategory || product.category
+        product.price = newPrice || product.price
+    }
+    
+    res.status(200).send("Atualização realizada com sucesso")
+})
 
 
 
